2cd636eed4969cf85d4651499f4f9b6c59a00032
Eric Normand <ericwnormand@gmail.com>
2015-02-22 17:16:05 -0600

    Create squat-test.

--- test/fitness/lower_body_test.clj
+++ test/fitness/lower_body_test.clj
@@ -2,3 +2,8 @@
   (:require
    [fitness.lower-body :refer :all]
    [clojure.test       :refer :all]))
+
+(deftest squat-test
+  (let [results (squats 50)]
+    (is (= 50 (:actual-reps results)))
+    (is (<= (:seconds results) 60))))

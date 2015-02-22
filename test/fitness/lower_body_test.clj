e47c3c9f579390b971a6d78fe5df1af3c015f569
Eric Normand <ericwnormand@gmail.com>
2015-02-22 17:16:12 -0600

    Create frog-leap-test.

--- test/fitness/lower_body_test.clj
+++ test/fitness/lower_body_test.clj
@@ -7,3 +7,8 @@
   (let [results (squats 50)]
     (is (= 50 (:actual-reps results)))
     (is (<= (:seconds results) 60))))
+
+(deftest frog-leap-test
+  (let [results (frog-leaps 5)]
+    (is (= 5 (:actual-reps results)))
+    (is (<= (:seconds results) 30))))

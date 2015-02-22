7bdf3869b3a9c2fbc32bb24e10467d5c9ea8c4a2
Eric Normand <ericwnormand@gmail.com>
2015-02-22 17:15:33 -0600

    Add pull-up-test.

--- test/fitness/upper_body_test.clj
+++ test/fitness/upper_body_test.clj
@@ -7,3 +7,8 @@
   (let [results (push-ups 50)]
     (is (= 50 (:actual-reps results)))
     (is (<= (:seconds results) (* 4 60)))))
+
+(deftest pull-up-test
+  (let [results (pull-ups 10)]
+    (is (= 10 (:actual-reps results)))
+    (is (<= (:seconds results) 60))))

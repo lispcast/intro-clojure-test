ff6f95a1a53ae13185e5d2a5f792de12670b03c6
Paul <paulll.lucas@gmail.com>
2016-10-31 15:40:54 -0700

    Use Clojure 1.8.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :target-path "target/%s"
   :aot [fitness.safety.UnsafeResistanceException]
   :profiles {:uberjar {:aot :all}})

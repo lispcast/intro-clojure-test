4b39428570f2bc1dc33c930179080965567a5441
Eric Normand <ericwnormand@gmail.com>
2019-01-30 10:44:32 -0600

    Update Clojure version to 1.10

--- project.clj
+++ project.clj
@@ -1,9 +1,15 @@
-(defproject fitness "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :target-path "target/%s"
-  :aot [fitness.safety.UnsafeResistanceException]
-  :profiles {:uberjar {:aot :all}})
+(defproject introduction-to-clojure "0.1.0-SNAPSHOT"
+  :description "Part of PurelyFunctional.tv"
+  :url "https://purelyfunctional.tv/"
+  :license {:name "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
+            :url "http://creativecommons.org/publicdomain/zero/1.0/"}
+  :plugins [[nightlight/lein-nightlight "2.3.2"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [lispcast/bakery "1.0.0"]]
+  :repl-options {:init (do
+                         (use 'bakery.core)
+                         (use 'introduction-to-clojure.core))
+                 :skip-default-init true}
+  
+
+  :main introduction-to-clojure.core)
